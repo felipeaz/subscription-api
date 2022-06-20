406172df0fe5e98b16647ee1f59fb5e036db7958
Felipe Azevedo <felipe9_azevedo@hotmail.com>
2022-06-20 13:04:04 -0300

    Initial code for topic handler

--- internal/app/domain/topic.go
+++ internal/app/domain/topic.go
@@ -1,35 +1,52 @@
 package domain
 
-type Topic interface {
-	Get() []topic
-	Find(id string) topic
-	Create(topic topic) (topic, error)
-	Update(id string, topic topic) error
-	Delete(id string) error
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"subscription-api/pkg/logger"
+)
+
+type TopicServer interface {
+	Get(ctx context.Context) ([]Topic, error)
+	Find(ctx context.Context, id string) (Topic, error)
+	Create(ctx context.Context, topicObj Topic) (Topic, error)
+	Update(ctx context.Context, id string, topic Topic) error
+	Delete(ctx context.Context, id string) error
+	NewTopicFromRequest(c *gin.Context) (Topic, error)
 }
 
-type topic struct {
+type Topic struct {
 	Name        string
 	Description string
 	Subscribers []*subscription
+	Log         *logger.Logger
 }
 
-func Get() []topic {
-	return nil
+func (t Topic) Get(ctx context.Context) ([]Topic, error) {
+	return nil, nil
 }
 
-func Find(id string) topic {
-	return topic{}
+func (t Topic) Find(ctx context.Context, id string) (Topic, error) {
+	return Topic{}, nil
 }
 
-func Create(t topic) (topic, error) {
+func (t Topic) Create(ctx context.Context, topicObj Topic) (Topic, error) {
 	return t, nil
 }
 
-func Update(id string, topic topic) error {
+func (t Topic) Update(ctx context.Context, id string, topic Topic) error {
 	return nil
 }
 
-func Delete(id string) error {
+func (t Topic) Delete(ctx context.Context, id string) error {
 	return nil
+}
+
+func (t Topic) NewTopicFromRequest(c *gin.Context) (Topic, error) {
+	var topicObj Topic
+	err := c.BindJSON(&topicObj)
+	if err != nil {
+		return Topic{}, err
+	}
+	return topicObj, nil
 }
--- internal/app/topic.go
+++ internal/app/topic.go
@@ -1,38 +1,74 @@
 package app
 
 import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"net/http"
 	"subscription-api/internal/app/domain"
-	"subscription-api/pkg/logger"
 )
 
 type Topic struct {
-	service domain.Topic
-	log     *logger.Logger
+	service domain.TopicServer
 }
 
-func NewTopic(service domain.Topic, log *logger.Logger) Topic {
+func NewTopic(service domain.TopicServer) Topic {
 	return Topic{
 		service: service,
-		log:     log,
 	}
 }
 
-func (t Topic) Get() {
-
+func (t Topic) Get(c *gin.Context) {
+	ctx := context.Background()
+	topics, err := t.service.Get(ctx)
+	if err != nil {
+		return
+	}
+	c.JSON(http.StatusOK, topics)
 }
 
-func (t Topic) Find() {
-
+func (t Topic) Find(c *gin.Context) {
+	ctx := context.Background()
+	id := c.Param("id")
+	topic, err := t.service.Find(ctx, id)
+	if err != nil {
+		return
+	}
+	c.JSON(http.StatusOK, topic)
 }
 
-func (t Topic) Create() {
-
+func (t Topic) Create(c *gin.Context) {
+	ctx := context.Background()
+	topic, err := t.service.NewTopicFromRequest(c)
+	if err != nil {
+		return
+	}
+	topicCreated, err := t.service.Create(ctx, topic)
+	if err != nil {
+		return
+	}
+	c.JSON(http.StatusOK, topicCreated)
 }
 
-func (t Topic) Update() {
-
+func (t Topic) Update(c *gin.Context) {
+	ctx := context.Background()
+	id := c.Param("id")
+	topic, err := t.service.NewTopicFromRequest(c)
+	if err != nil {
+		return
+	}
+	err = t.service.Update(ctx, id, topic)
+	if err != nil {
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
 }
 
-func (t Topic) Delete() {
-
+func (t Topic) Delete(c *gin.Context) {
+	ctx := context.Background()
+	id := c.Param("id")
+	err := t.service.Delete(ctx, id)
+	if err != nil {
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
 }
